06a0ea98f245a050e35970029ed0d6c44509cfe0
akma adhwa <muhdakma63@gmail.com>
2020-12-25 18:33:34 +0800

    Add login route

---
+++ app/Http/Controllers/api/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\LoginRequest;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $req)
+    {
+        $credentials = $req->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+
+            $accessToken = Auth::user()->createToken('authToken')->accessToken;
+
+            return response()->json([
+                'success'=> true,
+                "access_token" => $accessToken
+            ]);
+        }
+
+        return response()->json([
+            'error'=> true,
+        ], 401);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', [AuthController::class, 'login']);
+
+Route::group(['prefix' => 'v1', 'middleware' => ['auth:api']], function () {
+    Route::get('test', function() {
+        dd('test');
+    });
 });
